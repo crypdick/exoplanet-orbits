f36893644d8f5f7261e8362cb26d91e54c41e5e6
Richard Decal <richard.decal@ncf.edu>
2018-03-31 00:12:08 -0400

    plotting w colors. animation is slow af

--- planet_orbit.py
+++ planet_orbit.py
@@ -24,29 +24,27 @@ app.layout = html.Div([
         dcc.Graph(id='planet-orbits')
     ])
 ])
-print("layout done")
 
-# def temp_to_color(temp):
+def temp_to_color(temp):
 #     TODO implement
-    # return 'white'
+    return 'rgba(132, 239, 241, 1)'
 
 
 def mk_trace_generator(planet, in_habitable_zone=False):
     period, semi, size, temp = planet
-    # color = temp_to_color(temp)
-    #
-    # if in_habitable_zone:
-    #     line_width = size * 0.1
-    # else:
-    #     line_width = 0
+    color = temp_to_color(temp)
+
+    if in_habitable_zone:
+        line_width = size * 0.1
+    else:
+        line_width = 0
 
     time = 0.
     while True:
         phase = 2. * np.pi * time / period
         xs, ys = semi * np.cos(phase), semi * np.sin(phase)
         time += 0.23
-        print("generated")
-        yield (xs, ys)
+        yield (xs, ys, size, color)
         # TODO test making the go downstairs
         # graph_object = go.Scatter(
         #     x=np.array(xs),
@@ -62,8 +60,8 @@ def mk_trace_generator(planet, in_habitable_zone=False):
         # yield graph_object
 
 
-planet1 = (1.5, 1, 30., 500.)
-planet2 = (4.1, 0.5, 30., 500.)
+planet1 = (6., 1, 15., 500.)
+planet2 = (12., 3.5, 15., 500.)
 planets = [planet1, planet2]
 n_planets = len(planets)
 
@@ -74,16 +72,21 @@ trace_generators = [mk_trace_generator(planet) for planet in planets]
     Output('planet-orbits', 'figure'),
     [Input('interval-component', 'n_intervals')])
 def move_planets(interval):
-    print("moving planets")
-    xs = np.empty(n_planets)
-    ys = np.empty(n_planets)
+    xs = np.empty(n_planets+1)
+    ys = np.empty(n_planets+1)
+    sizes = np.empty(n_planets+1)
+    colors = []
     payload = [gen.__next__() for gen in trace_generators]
     for i, load in enumerate(payload):
-        x,y = load
-        xs[i], ys[i] = x,y
+        x, y, size, color = load
+        xs[i], ys[i], sizes[i] = x,y,size
+        colors.append(color)
 
-    print(xs, ys)
     # host star
+    xs[-1] = 0
+    ys[-1] = 0
+    sizes[-1] = 100
+    colors.append('rgba(229, 196, 31, 1)')
     # traces.append(go.Scatter(
     #     x=np.array(0),
     #     y=np.array(0),
@@ -95,7 +98,8 @@ def move_planets(interval):
     trace = go.Scattergl(x=xs,
                          y=ys,
                          mode="markers",
-                         )
+                         marker=dict(size=sizes,
+                                     color=colors))
     layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)',
                        plot_bgcolor='rgba(0,0,0,0)',
                        xaxis=dict(showgrid=False,
@@ -103,29 +107,30 @@ def move_planets(interval):
                                   range=[-5, 5]),
                        yaxis=dict(showgrid=False,
                                   zeroline=False,
+                                  scaleanchor='x',  # equal axis ratios
                                   range=[-5, 5])
                        )
 
     return go.Figure(data= [trace], layout= layout)
 
 
-# external_css = [
-#     "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
-#     "https://cdn.rawgit.com/plotly/dash-app-stylesheets/737dc4ab11f7a1a8d6b5645d26f69133d97062ae/dash-wind-streaming.css",
-#     "https://fonts.googleapis.com/css?family=Raleway:400,400i,700,700i",
-#     "https://fonts.googleapis.com/css?family=Product+Sans:400,400i,700,700i"]
-#
-# for css in external_css:
-#     app.css.append_css({"external_url": css})
-#
-# app.css.append_css({'external_url': 'https://codepen.io/plotly/pen/YeqjLb.css'})
-# app.css.append_css({
-#                        'external_url': 'https://raw.githubusercontent.com/crypdick/exoplanet-orbits/master/exoplanet.css'})
-#
-# if 'DYNO' in os.environ:
-#     app.scripts.append_script({
-#         'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
-#     })
+external_css = [
+    "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
+    "https://cdn.rawgit.com/plotly/dash-app-stylesheets/737dc4ab11f7a1a8d6b5645d26f69133d97062ae/dash-wind-streaming.css",
+    "https://fonts.googleapis.com/css?family=Raleway:400,400i,700,700i",
+    "https://fonts.googleapis.com/css?family=Product+Sans:400,400i,700,700i"]
+
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
+app.css.append_css({'external_url': 'https://codepen.io/plotly/pen/YeqjLb.css'})
+app.css.append_css({
+                       'external_url': 'https://raw.githubusercontent.com/crypdick/exoplanet-orbits/master/exoplanet.css'})
+
+if 'DYNO' in os.environ:
+    app.scripts.append_script({
+        'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
+    })
 #
 if __name__ == '__main__':
     app.run_server()
